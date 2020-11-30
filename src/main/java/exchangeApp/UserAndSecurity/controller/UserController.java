836a4987ad7d0100004414563ca1e039f80a6bd9
br_se@mail.ru <BronZEseAl132>
2020-11-30 03:26:19 +0300

    Started working on Angular frontend

--- src/main/java/exchangeApp/UserAndSecurity/controller/UserController.java
+++ src/main/java/exchangeApp/UserAndSecurity/controller/UserController.java
@@ -4,15 +4,12 @@ import exchangeApp.UserAndSecurity.entity.DTO.NewUserDTO;
 import exchangeApp.UserAndSecurity.entity.DTO.UserInfoDTO;
 import exchangeApp.UserAndSecurity.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin
-@Controller
+@RestController
 public class UserController {
     private final UserService service;
 
